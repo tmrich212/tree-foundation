490bdd9c73e49aa3735de2ab0ae28ffc08a05023
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-11-29 16:05:28 -0500

    Carousel functionality and Bootstrap files added

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Initialization for ES Users
+// import { Carousel, initMDB } from "mdb-ui-kit";
+
+// initMDB({ Carousel });
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
