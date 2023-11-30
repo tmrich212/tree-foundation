54bae82418d6e4ddd845653884d4d9e941330450
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-11-29 20:59:33 -0500

    Section one desktop done

--- scripts.js
+++ scripts.js
@@ -1,6 +1,2 @@
-// Initialization for ES Users
-// import { Carousel, initMDB } from "mdb-ui-kit";
-
-// initMDB({ Carousel });
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
