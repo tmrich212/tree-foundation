5aa289f33439980f4a8116684d7e87bd6deb74f6
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-12-03 14:18:55 -0500

    Form style

--- scripts.js
+++ scripts.js
@@ -7,13 +7,24 @@ const btn3 = document.querySelector('.btn3');
 const impactBox = document.querySelector('#stats-title');
 
 btn1.addEventListener('click', function(){
-    impactBox.innerHTML = '<h1 class="animate__animated animate__bounceInLeft">20,000 Trees Planted!</h1>'
+    impactBox.innerHTML = '<h1 class="animate__animated animate__bounceInLeft">As of 2023, we have planted 200,000 trees!</h1>'
 });
 
 btn2.addEventListener('click', () => {
-    impactBox.innerHTML = '<h1 class="animate__animated animate__bounceInUp">There have been a total of 1,200+ voluneteers in 2023</h1>'
+    impactBox.innerHTML = '<h1 class="animate__animated animate__bounceInUp">There have been a total of 1,200+ voluneteers in 2023!</h1>'
 })
 
 btn3.addEventListener('click', () => {
     impactBox.innerHTML = '<h1 class="animate__animated animate__bounceInRight">As of 2023, we have recieved $30,000 donations!</h1>'
 })
+
+const submit = document.querySelector('#submit');
+const input = 
+
+submit.addEventListener('click', submitClick, false);
+
+function submitClick(event){
+    submit.innerHTML = "Thank you for signing up :)"
+    submit.style.backgroundColor = 'orange'
+    event.preventDefault()
+}
